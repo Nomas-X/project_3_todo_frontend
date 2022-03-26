7ab8777751eafc24d81fc7f0b0e386759edceb20
Redwan S <nomasx2000@gmail.com>
2022-03-26 19:04:59 +0300

    Removed unneeded API call

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Home from './Pages/Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import NotFound from './Pages/NotFound';
-import Dashboard from './Pages/Dashboard';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { indigo } from '@mui/material/colors';
-import Layout from './Components/Layout';
+import Home from './Pages/Home';
+import Dashboard from './Pages/Dashboard';
 import Create from './Pages/Create';
+import NotFound from './Pages/NotFound';
 import Signout from './Pages/Signout';
 import ForgotPassword from './Pages/Forgotpassword';
+import Layout from './Components/Layout';
 
 const theme = createTheme({
 	palette: {
@@ -20,23 +20,6 @@ const theme = createTheme({
   })
 
 class App extends React.Component {
-	constructor(props){
-		super(props);
-		this.state = { apiResponse: "" };
-	}
-
-	CallAPI(){
-		fetch("http://localhost:9000/testAPI", {
-			withCredntials: true,
-			credentials: 'include'
-		})
-		.then(res => res.text())
-		.then(res => this.setState({ apiResponse: res }));
-	}
-
-	componentDidMount(){
-		this.CallAPI();
-	}
 
 	render(){
 		return (
@@ -72,7 +55,6 @@ class App extends React.Component {
 							</Layout>
 						</Route>
 					</Switch>
-					{/* <p>{this.state.apiResponse}</p> */}
 				</Router>
 			</ThemeProvider>
 		);
